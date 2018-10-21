f49610314a7ba56daddd07f0702163d0f13760f8
Nutrient <carlobruno_fb@hotmail.com>
2018-10-21 16:02:48 -0500

    fixed socket

--- index.js
+++ index.js
@@ -5,33 +5,33 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 const server = net.createServer((socket) => {
+//  socket.on('error', err => {})
 
-}).on('error', (err) => {
-  // handle errors here
-  throw err;
+  socket.on('error', (err) => {
+    // handle errors here
+    console.log(err);
+    throw err;
+  })
+  socket.on('connection', (data,err) => {
+    console.log('connection created');
+  })
+  socket.on('data', data => {
+    console.log('data', data);
+  })
+  socket.on('end', data => {
+    console.log('end', data);
+  })
+  socket.on('close', data => {
+    console.log('close', data);
+  })
 })
-server.on('connection', (data,err) => {
-  console.log('connection created');
-})
-
-server.on('data', data => {
-  console.log(data);
-})
-
-server.on('end', data => {
-  console.log('end', data);
-})
-
-server.on('close', data => {
-  console.log('close', data);
-})
-
 // grab an arbitrary unused port.
 server.listen({
-  host:'localhost',
+  host:'0.0.0.0',
   port: 5000}, () => {
   console.log('opened server on', server.address());
 });
+
 
 app.get('/',(req, res) => {
   res.status(200).send({msg:'hello from root'});
