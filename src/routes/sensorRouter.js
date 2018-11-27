00b71e354302f4139cd691a5284db3c4bfddaf14
Nutrient <carlobruno_fb@hotmail.com>
2018-11-27 05:26:08 -0600

    modified sensors

--- src/routes/sensorRouter.js
+++ src/routes/sensorRouter.js
@@ -7,10 +7,18 @@ sensorRouter.route('/air')
   .get((req, res) => {
       res.sendFile(path.resolve('public/html/air.html'))
   });
-  
+
 sensorRouter.route('/temp')
   .get((req, res) => {
       res.sendFile(path.resolve('public/html/temp.html'))
   });
+
+sensorRouter.route('/getTemp')
+  .get((req, res) => {
+
+  });
+
+
+
 
 module.exports = sensorRouter;
